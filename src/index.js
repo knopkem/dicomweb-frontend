d511d64d4b537409153bfec409b6fba434eb688a
Michael Knopke <knopkem@gmail.com>
2021-07-29 10:33:55 +0200

    removing sentry

--- src/index.js
+++ src/index.js
@@ -2,18 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactApp from './App';
 import * as serviceWorker from './serviceWorker';
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
-
-Sentry.init({
-  dsn: "https://3aacab02c26a439b8d41a99cf62587f3@o570431.ingest.sentry.io/5717246",
-  integrations: [new Integrations.BrowserTracing()],
-
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
 
 ReactDOM.render(
   <React.Fragment>
